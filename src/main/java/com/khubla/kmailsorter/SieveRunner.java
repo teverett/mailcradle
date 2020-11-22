0c7502dc6aa325001055be04563a1ea74bdb2bcb
Tom Everett <tom@khubla.com>
2020-11-21 17:40:32 -0700

    better

--- src/main/java/com/khubla/kmailsorter/SieveRunner.java
+++ src/main/java/com/khubla/kmailsorter/SieveRunner.java
@@ -2,6 +2,8 @@ package com.khubla.kmailsorter;
 
 import java.io.*;
 
+import javax.mail.*;
+
 import org.apache.logging.log4j.*;
 
 import com.khubla.kmailsorter.domain.*;
@@ -12,7 +14,18 @@ public class SieveRunner {
 	 */
 	private static final Logger logger = LogManager.getLogger(SieveRunner.class);
 
+	private Folder getInbox() throws MessagingException {
+		final Session session = Session.getDefaultInstance(System.getProperties(), null);
+		final Store store = session.getStore("imaps");
+		store.connect(KMailSorterConfiguration.getInstance().getImapHost(), KMailSorterConfiguration.getInstance().getImapUsername(), KMailSorterConfiguration.getInstance().getImapPassword());
+		return store.getDefaultFolder();
+	}
+
+	private void runRules(Folder inbox, Sieve sieve) {
+	}
+
 	public void runSieveFile(File sieveFile) {
+		Folder inbox = null;
 		try {
 			/*
 			 * read sieve file
@@ -21,8 +34,24 @@ public class SieveRunner {
 			if (null != sieve) {
 				System.out.println("Read sieve file: " + sieveFile.getName());
 			}
+			/*
+			 * get inbox
+			 */
+			inbox = getInbox();
+			/*
+			 * run rules
+			 */
+			runRules(inbox, sieve);
 		} catch (final Exception e) {
 			logger.error(e);
+		} finally {
+			if (null != inbox) {
+				try {
+					inbox.close();
+				} catch (final Exception e) {
+					logger.info(e);
+				}
+			}
 		}
 	}
 }
