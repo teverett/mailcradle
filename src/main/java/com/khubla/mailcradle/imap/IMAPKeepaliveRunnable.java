4fd8c954b7a533d887b4c947f026d9cda831590b
Tom Everett <tom@khubla.com>
2020-12-01 19:44:37 -0700

    better keepalive

--- src/main/java/com/khubla/mailcradle/imap/IMAPKeepaliveRunnable.java
+++ src/main/java/com/khubla/mailcradle/imap/IMAPKeepaliveRunnable.java
@@ -5,9 +5,7 @@ import javax.mail.*;
 import org.apache.logging.log4j.*;
 
 import com.khubla.mailcradle.*;
-import com.sun.mail.iap.*;
 import com.sun.mail.imap.*;
-import com.sun.mail.imap.protocol.*;
 
 /**
  * https://stackoverflow.com/questions/4155412/javamail-keeping-imapfolder-idle-alive
@@ -35,14 +33,7 @@ public class IMAPKeepaliveRunnable implements Runnable {
 	public void run() {
 		while (!Thread.interrupted()) {
 			try {
-				logger.info("Keepalive");
-				imapFolder.doCommand(new IMAPFolder.ProtocolCommand() {
-					@Override
-					public Object doCommand(IMAPProtocol p) throws ProtocolException {
-						p.simpleCommand("NOOP", null);
-						return null;
-					}
-				});
+				logger.info("Keepalive.  Message count: " + imapFolder.getMessageCount());
 				Thread.sleep(keepalive);
 			} catch (final InterruptedException e) {
 				/*
