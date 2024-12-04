5d7b54fe4ff7845fc584616888ce3b4d1a88df68
Tom Everett <tom@khubla.com>
2024-12-03 18:37:16 -0700

    fix

--- src/main/java/com/khubla/mailcradle/domain/action/StopAction.java
+++ src/main/java/com/khubla/mailcradle/domain/action/StopAction.java
@@ -1,26 +1,26 @@
 package com.khubla.mailcradle.domain.action;
 
-import javax.mail.*;
+import com.khubla.mailcradle.domain.Action;
+import com.khubla.mailcradle.domain.Mailcradle;
+import com.khubla.mailcradle.imap.IMAPMessageData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import org.apache.logging.log4j.*;
-
-import com.khubla.mailcradle.domain.*;
-import com.khubla.mailcradle.imap.*;
-
+import javax.mail.MessagingException;
 import java.util.List;
 
 public class StopAction extends Action {
-	/**
-	 * logger
-	 */
-	private static final Logger logger = LogManager.getLogger(StopAction.class);
+   /**
+    * logger
+    */
+   private static final Logger logger = LogManager.getLogger(StopAction.class);
 
-	@Override
-	public boolean execute(IMAPMessageData messageData, Mailcradle mailsort, List<String> expressionSteps) throws MessagingException {
-		dumpExpressionSteps(expressionSteps);
-		// System.out.println("Stopping rule processing for message " + messageData.getId() + " in
-		// folder " + messageData.getFolderName());
-		logger.info("\tStopping rule processing for message " + messageData.getId() + " in folder " + messageData.getFolderName());
-		return false;
-	}
+   @Override
+   public boolean execute(IMAPMessageData messageData, Mailcradle mailsort, List<String> expressionSteps) throws MessagingException {
+      //dumpExpressionSteps(expressionSteps);
+      // System.out.println("Stopping rule processing for message " + messageData.getId() + " in
+      // folder " + messageData.getFolderName());
+      logger.info("\tStopping rule processing for message " + messageData.getId() + " in folder " + messageData.getFolderName());
+      return false;
+   }
 }
